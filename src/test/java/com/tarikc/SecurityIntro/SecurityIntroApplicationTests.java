abb4145cad646c210ccee3b8860b73d6550d2970
TarikC97 <tarikcuric97@gmail.com>
2024-06-03 01:46:20 +0200

    SecurityIntro

---
+++ src/test/java/com/tarikc/SecurityIntro/SecurityIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarikc.SecurityIntro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
